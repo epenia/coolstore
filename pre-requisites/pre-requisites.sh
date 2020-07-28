a0a95752a244d60ce917723d21c12c6ff99f9e3b
Luis Arizmendi <larizmen@hal9k.local>
2020-07-28 13:27:09 +0200

    pre-requisites script

--- pre-requisites/pre-requisites.sh
+++ pre-requisites/pre-requisites.sh
@@ -2,8 +2,47 @@
 
 
 
+########################
+# Workspaces
+#######################
+cd workspaces
+chmod +x run.sh
+./run.sh
+cd ..
 
 
 
+########################
+# Service Mesh
+#######################
+cd service-mesh
+chmod +x run.sh
+./run.sh
+cd ..
 
 
+########################
+# Knative
+#######################
+cd knative
+chmod +x run.sh
+./run.sh
+cd ..
+
+
+########################
+# Knative Kafka
+#######################
+cd knative_kafka
+chmod +x run.sh
+./run.sh
+cd ..
+
+
+########################
+# AMQ
+#######################
+cd amq_streams
+chmod +x run.sh
+./run.sh
+cd ..
