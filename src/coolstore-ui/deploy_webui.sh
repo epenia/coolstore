fe2cf54241e591441840bc64e3b6bd0f98365eae
Luis Arizmendi <larizmen@hal9k.local>
2020-07-29 18:04:31 +0200

    Add HPA on web UI

--- src/coolstore-ui/deploy_webui.sh
+++ src/coolstore-ui/deploy_webui.sh
@@ -12,3 +12,26 @@ oc label dc/coolstore-ui app.kubernetes.io/part-of=coolstore --overwrite && \
 oc annotate dc/coolstore-ui app.openshift.io/connects-to=order-cart,catalog,inventory,order --overwrite && \
 oc annotate dc/coolstore-ui app.openshift.io/vcs-uri=https://https://github.com/luisarizmendi/coolstore.git --overwrite && \
 oc annotate dc/coolstore-ui app.openshift.io/vcs-ref=ocp-4.4 --overwrite
+
+
+
+oc create -f - <<EOF
+apiVersion: autoscaling/v2beta2
+kind: HorizontalPodAutoscaler
+metadata:
+  name: coolstore-ui-cpu-autoscale
+spec:
+  scaleTargetRef:
+    apiVersion: apps.openshift.io/v1
+    kind: DeploymentConfig
+    name: coolstore-ui
+  minReplicas: 1
+  maxReplicas: 10
+  metrics:
+  - type: Resource
+    resource:
+      name: cpu
+      target:
+        type: Utilization
+        averageValue: 200m
+EOF
