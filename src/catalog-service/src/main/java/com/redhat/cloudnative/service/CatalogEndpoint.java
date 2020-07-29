2812a1bfba04bc33d2caa3a8e8ae6cdb82922f8f
Luis Arizmendi <larizmen@hal9k.local>
2020-07-29 15:47:46 +0200

    Removing custom mon due problems

--- src/catalog-service/src/main/java/com/redhat/cloudnative/service/CatalogEndpoint.java
+++ src/catalog-service/src/main/java/com/redhat/cloudnative/service/CatalogEndpoint.java
@@ -23,7 +23,6 @@ public class CatalogEndpoint {
     @GetMapping("/products")
     @CrossOrigin
     public ResponseEntity<List<Product>> readAll() {
-        Metrics.counter("api.catalog.readall.total").increment(1.0);
         return new ResponseEntity<List<Product>>(catalogService.readAll(),HttpStatus.OK);
     }
 
